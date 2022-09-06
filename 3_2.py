8284eeefc465a1b3924f88cdec61605497bbe064
REZHED <sairus.hedayat@gmail.com>
2022-09-07 00:21:22 +0300

    add solution

--- 3_2.py
+++ 3_2.py
@@ -1,1 +1,6 @@
 #Пользователь вводит 3 числа, найти среднее арифмитическое с точностью 3
+first_number = float(input("введите 1 число: "))
+second_number = float(input("введите 2 число: "))
+third_number = float(input("введите 2 число: "))
+print(round((first_number + second_number + third_number)/3, 3))
+
