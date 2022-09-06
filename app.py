b6f89c82caf57ae36fa2c0cb63bb5c10a9266d82
REZAHED <sairus.hedayat@gmail.com>
2022-09-06 15:29:30 +0300

    edit code

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 name = input("Как тебя зовут? ")
 letter = "буквы"
 letter2 = "букв"
+list = []
 def func(name):
     i = len(name)
     conut = 0
-    if " " in name:
-        conut += 1
+    if ' ' in name:
         for i in name:
+            list.append(i)
             print(i)
         print("""_________________________________________""")
+        print(list)
+        for space in list:
+            if space == ' ':
+                conut += 1
         if len(name) <= 4:
             print("длина этого имя состоит из ", len(name)-conut, letter)
         else:
