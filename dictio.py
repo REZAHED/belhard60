04009530a6e92e01bbdb71709f48ed8448b43d4a
REZAHED <sairus.hedayat@gmail.com>
2022-09-07 11:42:36 +0300

    new file

--- dictio.py
+++ dictio.py
@@ -3,12 +3,12 @@ file_content = txt_file.read().lower()
 content_list = file_content.split("\n")
 txt_file.close()
 word_number = 0
-count=1
+count = 1
 while word_number <= len(content_list)-1:
     word = content_list[word_number]
     word_reverse = word[::-1]
-    if word == word_reverse:
-        print(count,word)
+    if word == word_reverse and len(word) >= 3:
+        print(count, word)
         word_number += 1
         count += 1
     else:
