007c17958164af995c29d5fd4edfbe7e00c63f91
REZAHED <sairus.hedayat@gmail.com>
2022-09-07 10:59:57 +0300

    new file

--- dictio.py
+++ dictio.py
@@ -5,8 +5,13 @@ file_content = txt_file.read()
 content_list = file_content.split("\n")
 txt_file.close()
 word_number = 0
-for i in content_list:
+a=0
+print(len(content_list))
+while a<= len(content_list):
     if content_list[word_number]==content_list[word_number::-1]:
         print(content_list[word_number])
-        i+=1
+    else:
+        print("не нашел в", content_list[word_number])
+        a+=1
+        word_number+=1
 
