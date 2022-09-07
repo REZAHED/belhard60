4a269eb37aae3c940d700c6e3c53e45324d7b0da
REZAHED <sairus.hedayat@gmail.com>
2022-09-07 12:38:59 +0300

    add solution

--- 3_4.py
+++ 3_4.py
@@ -1,2 +1,10 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных
 # и сколько отрицательных
+first_number = input("Введите первое число: ")
+second_number = input("Введите второе число: ")
+third_number = input("Введите третье число: ")
+text = first_number.startswith("-")+second_number.startswith("-")+third_number.startswith("-")
+print("количество положительных чисел:", 3-int(text),"\nколичество отрицательных чисел:",text)
+
+
+
