3e94205709be48745aef77c008681c3fccece2aa
REZAHED <sairus.hedayat@gmail.com>
2022-09-08 09:19:24 +0300

    add solution

--- 3_4.py
+++ 3_4.py
@@ -4,7 +4,9 @@ first_number = input("Введите первое число: ")
 second_number = input("Введите второе число: ")
 third_number = input("Введите третье число: ")
 text = first_number.startswith("-") + second_number.startswith("-") + third_number.startswith("-")
-print("количество положительных чисел:", 3 - int(text), "\nколичество отрицательных чисел:", text)
+text_2 = first_number.count("0") + second_number.count("0") + third_number.count("0")
+print("количество положительных чисел:", 3 - int(text) - int(text_2), "\nколичество отрицательных чисел:", text)
+print("количество число 0: ", text_2)
 
 
 
