1e5ce2c270423610489d08b3958af206f2f5e7a9
REZHED <sairus.hedayat@gmail.com>
2022-09-14 22:30:18 +0300

    add solution

--- 4_1.py
+++ 4_1.py
@@ -1,6 +1,6 @@
 #Заполнить список степенями числа 2 (от 2^1 до 2^n).
 
-number = int(input("введите число: "))
-lst = [2**i for i in range(1, number+1)]
+number = int(input("введите число n: "))
+lst = [2**n for n in range(1, number+1)]
 print(lst)
 
