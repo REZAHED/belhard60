7e558fe92f1f36f6af74477d5653a4037ff3452b
Timur <mr.tiger171@mail.ru>
2022-09-15 21:09:36 +0300

    add print

--- test3.py
+++ test3.py
@@ -1,8 +1,18 @@
 text = input()
 lst = list(text)
 count = 0
-for i in range(0, len(lst)-1):
-    if lst[i] == lst[i+1]:
-        count += 1
+i = 0
+
+while i < len(lst)-1:
+    if lst[i] == text[i+1]:
+        count+=1
+        i+=2
+    else:
+        i+=1
 print(count)
+
+# for i in range(0, len(lst)-1):
+#     if lst[i] == lst[i+1]:
+#         count += 1
+# print(count)
 
---
+++ test4.py
@@ -0,0 +1,8 @@
+x = int(input("x "))
+y = int(input("y "))
+c = 0
+while x < y:
+    x += x/10
+    c += 1
+print(c+1)
+
