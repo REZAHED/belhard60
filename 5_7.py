74ec763a6e4d1272e8f09a2c28bc03d33bdc0c2d
REZAHED <sairus.hedayat@gmail.com>
2022-09-19 17:44:09 +0300

    add solution

--- 5_7.py
+++ 5_7.py
@@ -1,5 +1,7 @@
 # Дан список чисел, отсортировать его по возрастанию без использования sort и sorted
 number = [2, 6, 4, 1, 5, 16, 3, 9, 12, 7]
 number_sort = set(number)
-print(number_sort)
-
+number_lst = []
+for i, j in enumerate(number_sort):
+    number_lst.append(j)
+print(number_lst)
