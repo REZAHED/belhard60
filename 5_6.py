2744eb6e23686051f1662a424c8efebc9cc20f73
REZAHED <sairus.hedayat@gmail.com>
2022-09-20 13:03:13 +0300

    add solution

--- 5_6.py
+++ 5_6.py
@@ -1,24 +1,15 @@
 # Вводятся две строки, a и b, и возвращать количество раз,
 # когда в обеих строках под одинаковыми индексами стоит одна и та же пара букв.
 
-first = input("введите текст: ").lower()
-second = input("введите второй текст: ").lower()
-first2 = first.replace(" ","")
-second2=second.replace(" ","")
+first = input("введите текст: ").lower().replace(" ","")
+second = input("введите второй текст: ").lower().replace(" ","")
 c = 0
-for i in range(0,len(first2)):
-    if len(first2)<=len(second2):
-        if first2[i] == second2[i]:
-            c += 1
-            print(f"Буква {first2[i]} Индекс:{i}", end=" ")
-    else:
+len_ = 0
+for i in range(0, min(len(first),len(second))):
+  if first[i] == second[i]:
+    c += 1
+    print(f"Буква {first[i]} Индекс:{i}", end=" ")
+print("\n", "количество :",c)
 
-print("\n", "количество одинаковых пар: ",c)
-# else:
-#     for i in range(0,len(second2)):
-#         if first2[i] == second2[i]:
-#             c += 1
-#             print("Буква",first2[i]," Индекс:",i," ", end=" ")
-#     print("\n", "количество одинаковых пар: ",c)
 
 
