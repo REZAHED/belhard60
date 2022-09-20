f35a73e92d49091c5b81c0bcaea6179e9bd4b85c
REZAHED <sairus.hedayat@gmail.com>
2022-09-20 11:59:31 +0300

    add solution

--- 5_6.py
+++ 5_6.py
@@ -5,18 +5,20 @@ first = input("введите текст: ").lower()
 second = input("введите второй текст: ").lower()
 first2 = first.replace(" ","")
 second2=second.replace(" ","")
-c=0
-if len(first2) <= len(second2):
-    for i in range(0,len(first2)):
+c = 0
+for i in range(0,len(first2)):
+    if len(first2)<=len(second2):
         if first2[i] == second2[i]:
-            c+=1
-            print("Буква",first2[i]," Индекс:",i," ", end=" ")
-    print("\n", "количество одинаковых пар: ",c)
-else:
-    for i in range(0,len(second2)):
-        if first2[i] == second2[i]:
-            c+=1
-            print("Буква",first2[i]," Индекс:",i," ", end=" ")
-    print("\n", "количество одинаковых пар: ",c)
+            c += 1
+            print(f"Буква {first2[i]} Индекс:{i}", end=" ")
+    else:
+
+print("\n", "количество одинаковых пар: ",c)
+# else:
+#     for i in range(0,len(second2)):
+#         if first2[i] == second2[i]:
+#             c += 1
+#             print("Буква",first2[i]," Индекс:",i," ", end=" ")
+#     print("\n", "количество одинаковых пар: ",c)
 
 
--- 5_7.py
+++ 5_7.py
@@ -1,8 +1,10 @@
 # Дан список чисел, отсортировать его по возрастанию без использования sort и sorted
-number = [2, 6, 4, 1, 5, 16, 3, 9, 12, 7]
-number_sort = set(number)
+number = [2, 2, 6, 4, 8, 1, 5, 16, 3, 9, 12, 7 , 45, 13, 76, 4]
 number_lst = []
-for i, j in enumerate(number_sort):
-    number_lst.append(j)
-print("Input: ", number)
-print("Sorted:",number_lst)
+for i in range(0,len(number)):
+    number_lst.append(min(number))
+    number.pop(number.index(min(number)))
+print(number_lst)
+
+
+
