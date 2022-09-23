14408a95a92feca049d178b4d7b1c66306bae7a1
REZAHED <sairus.hedayat@gmail.com>
2022-09-23 17:19:22 +0300

    add solution

--- 6_6.py
+++ 6_6.py
@@ -1,3 +1,24 @@
 # Дан список рандомных чисел, необходимо отсортировать его в виде, сначала
 # четные, потом нечётные
+import random
+from random import randint
+number = []
 
+
+def rand(number:list):          # создать рандомный список
+    number.clear()
+    for i in range(0, 10):
+        number.append(random.randint(0, 100))
+    return number
+
+def odd():
+    print(f"первоначвльный рандомный список :{rand(number)}")
+    number2 =[]
+    number2 = list(filter(lambda x:x%2==0 ,(number)))
+
+    for i in range(0,len(number)):
+        if number[i]%2:
+            number2.append(number[i])
+    return number2
+
+print("\t"*4,f"отсортированный:{odd()}")
