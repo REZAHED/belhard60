c9f1a1e0427db31781acf0f748e1b111ab067bb4
REZAHED <sairus.hedayat@gmail.com>
2022-09-26 16:09:12 +0300

    add solution

--- 6_3.py
+++ 6_3.py
@@ -2,9 +2,4 @@
 # указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
 lst = [1, 2, 3, 4, 5, 6, 7]
 number = int(input("Ведите число: "))
-lst2=[]
-for i in range(len(lst)-number,len(lst)):
-    lst2.append(lst[i])
-for i in range(0,len(lst)-number):
-    lst2.append(lst[i])
-print(lst2)
+print(lst[len(lst)-number:] + lst[0:len(lst)-number])
