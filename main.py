1c7ed063010143b2285b6ca6bf3656c36710adb7
davydovbv <bogedavydov@gmail.com>
2022-09-27 21:30:15 +0300

    print

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+# number = input("введите число: ")
+
+def foo(number):
+    while len(number) > 1:
+        s=0
+        for i in number:
+            s+= int(i)
+        number = str(s)
+    return int(number)
+print(foo("759"))
+
+
